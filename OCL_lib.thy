0acf07d8ccaddb87b8b444812c0dad7862cd3ceb
Burkhart Wolff <Burkharrt-Wolff@users.noreply.github.com>
2011-05-19 16:30:53 +0000

    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@9172 3260e6d1-4efc-4170-b0a7-36055960796d

---
+++ OCL_lib.thy
@@ -0,0 +1,168 @@
+theory OCL_lib
+imports Mini_OCL
+begin
+
+
+syntax
+  "notequal"        :: "('\<AA>)Boolean \<Rightarrow> ('\<AA>)Boolean \<Rightarrow> ('\<AA>)Boolean"   (infix "<>" 40)
+translations
+  "a <> b" == "CONST not( a \<doteq> b)"
+
+defs   StrictRefEq_int : "(x::('\<AA>,int)val) \<doteq> y \<equiv>
+                             \<lambda> \<tau>. if (\<delta> x) \<tau> = true \<tau> \<and> (\<delta> y) \<tau> = true \<tau>
+                                  then (x \<triangleq> y)\<tau>
+                                  else invalid \<tau>"
+
+
+defs   StrictRefEq_bool : "(x::('\<AA>,bool)val) \<doteq> y \<equiv>
+                             \<lambda> \<tau>. if (\<delta> x) \<tau> = true \<tau> \<and> (\<delta> y) \<tau> = true \<tau>
+                                  then (x \<triangleq> y)\<tau>
+                                  else invalid \<tau>"
+
+
+lemma StrictRefEq_int_strict1[simp] : "((x::('\<AA>,int)val) \<doteq> invalid) = invalid"
+by(rule ext, simp add: StrictRefEq_int true_def false_def)
+
+lemma StrictRefEq_int_strict2[simp] : "(invalid \<doteq> (x::('\<AA>,int)val)) = invalid"
+by(rule ext, simp add: StrictRefEq_int true_def false_def)
+
+lemma StrictRefEq_int_strict3[simp] : "((x::('\<AA>,int)val) \<doteq> null) = invalid"
+by(rule ext, simp add: StrictRefEq_int true_def false_def)
+
+lemma StrictRefEq_int_strict4[simp] : "(null \<doteq> (x::('\<AA>,int)val)) = invalid"
+by(rule ext, simp add: StrictRefEq_int true_def false_def)
+
+
+lemma strictEqBool_vs_strongEq: 
+"\<tau> \<Turnstile>(\<delta> x) \<Longrightarrow> \<tau> \<Turnstile>(\<delta> y) \<Longrightarrow> (\<tau> \<Turnstile> ((x::('\<AA>,bool)val) \<doteq> y)) = (\<tau> \<Turnstile> (x \<triangleq> y))"
+by(simp add: StrictRefEq_bool OclValid_def)
+
+lemma strictEqInt_vs_strongEq: 
+"\<tau> \<Turnstile>(\<delta> x) \<Longrightarrow> \<tau> \<Turnstile>(\<delta> y) \<Longrightarrow> (\<tau> \<Turnstile> ((x::('\<AA>,int)val) \<doteq> y)) = (\<tau> \<Turnstile> (x \<triangleq> y))"
+by(simp add: StrictRefEq_int OclValid_def)
+
+
+lemma strictEqBool_defargs: 
+"\<tau> \<Turnstile> ((x::('\<AA>,bool)val) \<doteq> y) \<Longrightarrow> (\<tau> \<Turnstile>(\<delta> x)) \<and> (\<tau> \<Turnstile>(\<delta> y))"
+by(simp add: StrictRefEq_bool OclValid_def true_def invalid_def
+           split: bool.split_asm HOL.split_if_asm)
+
+lemma strictEqInt_defargs: 
+"\<tau> \<Turnstile> ((x::('\<AA>,int)val) \<doteq> y)\<Longrightarrow> (\<tau> \<Turnstile>(\<delta> x)) \<and> (\<tau> \<Turnstile>(\<delta> y))"
+by(simp add: StrictRefEq_int OclValid_def true_def invalid_def
+           split: bool.split_asm HOL.split_if_asm)
+
+lemma gen_ref_eq_defargs: 
+"\<tau> \<Turnstile> (gen_ref_eq x (y::('\<AA>,'a::object)val))\<Longrightarrow> (\<tau> \<Turnstile>(\<delta> x)) \<and> (\<tau> \<Turnstile>(\<delta> y))"
+by(simp add: gen_ref_eq_def OclValid_def true_def invalid_def
+           split: bool.split_asm HOL.split_if_asm)
+
+
+lemma StrictRefEq_int_strict :
+  assumes A: "\<delta> (x::('\<AA>,int)val) = true"
+  and     B: "\<delta> y = true"
+  shows   "\<delta> (x \<doteq> y) = true"
+  apply(insert A B)
+  apply(rule ext, simp add: StrongEq_def StrictRefEq_int true_def defined_def)
+  done
+
+
+lemma StrictRefEq_int_strict' :
+  assumes A: "\<delta> ((x::('\<AA>,int)val) \<doteq> y) = true"
+  shows      "\<delta> x = true \<and> \<delta> y = true"
+  apply(insert A, rule conjI) 
+  apply(rule ext, drule_tac x=xa in fun_cong)
+  prefer 2
+  apply(rule ext, drule_tac x=xa in fun_cong)
+  apply(simp_all add: StrongEq_def StrictRefEq_int 
+                            false_def true_def defined_def)
+  apply(case_tac "y xa", auto)
+  apply(simp_all add: true_def invalid_def)
+  apply(case_tac "aa", auto simp:true_def false_def invalid_def 
+                            split: option.split option.split_asm)
+  done
+
+
+
+lemma StrictRefEq_bool_strict1[simp] : "((x::('\<AA>,bool)val) \<doteq> invalid) = invalid"
+by(rule ext, simp add: StrictRefEq_bool true_def false_def)
+
+lemma StrictRefEq_bool_strict2[simp] : "(invalid \<doteq> (x::('\<AA>,bool)val)) = invalid"
+by(rule ext, simp add: StrictRefEq_bool true_def false_def)
+
+lemma StrictRefEq_bool_strict3[simp] : "((x::('\<AA>,bool)val) \<doteq> null) = invalid"
+by(rule ext, simp add: StrictRefEq_bool true_def false_def)
+
+lemma StrictRefEq_bool_strict4[simp] : "(null \<doteq> (x::('\<AA>,bool)val)) = invalid"
+by(rule ext, simp add: StrictRefEq_bool true_def false_def)
+
+lemma cp_StrictRefEq_bool: 
+"((X::('\<AA>,bool)val) \<doteq> Y) \<tau> = ((\<lambda> _. X \<tau>) \<doteq> (\<lambda> _. Y \<tau>)) \<tau>"
+by(auto simp: StrictRefEq_bool StrongEq_def invalid_def  cp_defined[symmetric])
+
+lemma cp_StrictRefEq_int: 
+"((X::('\<AA>,int)val) \<doteq> Y) \<tau> = ((\<lambda> _. X \<tau>) \<doteq> (\<lambda> _. Y \<tau>)) \<tau>"
+by(auto simp: StrictRefEq_int StrongEq_def invalid_def  cp_defined[symmetric])
+
+
+
+lemmas H =
+       cp_StrictRefEq_bool[THEN allI[THEN allI[THEN allI[THEN cpI2]], 
+             of "StrictRefEq"]]
+       cp_StrictRefEq_int[THEN allI[THEN allI[THEN allI[THEN cpI2]], 
+             of "StrictRefEq"]]
+
+
+lemma StrictRefEq_strict :
+  assumes A: "\<delta> (x::('\<AA>,int)val) = true"
+  and     B: "\<delta> y = true"
+  shows   "\<delta> (x \<doteq> y) = true"
+  apply(insert A B)
+  apply(rule ext, simp add: StrongEq_def StrictRefEq_int true_def defined_def)
+  done
+
+
+definition ocl_zero ::"('\<AA>)Integer" ("\<zero>")
+where      "\<zero> = (\<lambda> _ . \<lfloor>\<lfloor>0::int\<rfloor>\<rfloor>)"
+
+definition ocl_one ::"('\<AA>)Integer" ("\<one> ")
+where      "\<one>  = (\<lambda> _ . \<lfloor>\<lfloor>1::int\<rfloor>\<rfloor>)"
+
+definition ocl_two ::"('\<AA>)Integer" ("\<two>")
+where      "\<two> = (\<lambda> _ . \<lfloor>\<lfloor>2::int\<rfloor>\<rfloor>)"
+
+definition ocl_three ::"('\<AA>)Integer" ("\<three>")
+where      "\<three> = (\<lambda> _ . \<lfloor>\<lfloor>3::int\<rfloor>\<rfloor>)"
+
+definition ocl_four ::"('\<AA>)Integer" ("\<four>")
+where      "\<four> = (\<lambda> _ . \<lfloor>\<lfloor>4::int\<rfloor>\<rfloor>)"
+
+definition ocl_five ::"('\<AA>)Integer" ("\<five>")
+where      "\<five> = (\<lambda> _ . \<lfloor>\<lfloor>5::int\<rfloor>\<rfloor>)"
+
+definition ocl_six ::"('\<AA>)Integer" ("\<six>")
+where      "\<six> = (\<lambda> _ . \<lfloor>\<lfloor>6::int\<rfloor>\<rfloor>)"
+
+definition ocl_seven ::"('\<AA>)Integer" ("\<seven>")
+where      "\<seven> = (\<lambda> _ . \<lfloor>\<lfloor>7::int\<rfloor>\<rfloor>)"
+
+definition ocl_eight ::"('\<AA>)Integer" ("\<eight>")
+where      "\<eight> = (\<lambda> _ . \<lfloor>\<lfloor>8::int\<rfloor>\<rfloor>)"
+
+definition ocl_nine ::"('\<AA>)Integer" ("\<nine>")
+where      "\<nine> = (\<lambda> _ . \<lfloor>\<lfloor>9::int\<rfloor>\<rfloor>)"
+
+
+definition ocl_less_int ::"('\<AA>)Integer \<Rightarrow> ('\<AA>)Integer \<Rightarrow> ('\<AA>)Boolean" (infix "\<prec>" 40) 
+where "x \<prec> y \<equiv> \<lambda> \<tau>. if (\<delta> x) \<tau> = true \<tau> \<and> (\<delta> y) \<tau> = true \<tau>
+                then \<lfloor>\<lfloor>\<lceil>\<lceil>x \<tau>\<rceil>\<rceil> < \<lceil>\<lceil>y \<tau>\<rceil>\<rceil>\<rfloor>\<rfloor>
+                else invalid \<tau> "   
+
+definition ocl_le_int ::"('\<AA>)Integer \<Rightarrow> ('\<AA>)Integer \<Rightarrow> ('\<AA>)Boolean" (infix "\<preceq>" 40) 
+where "x \<preceq> y \<equiv> \<lambda> \<tau>. if (\<delta> x) \<tau> = true \<tau> \<and> (\<delta> y) \<tau> = true \<tau>
+                then \<lfloor>\<lfloor>\<lceil>\<lceil>x \<tau>\<rceil>\<rceil> \<le> \<lceil>\<lceil>y \<tau>\<rceil>\<rceil>\<rfloor>\<rfloor>
+                else invalid \<tau> "   
+
+
+
+end
