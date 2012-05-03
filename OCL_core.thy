4ec0e96495a4052b81a5938ffee025d72fd5ee74
Burkhart Wolff <Burkharrt-Wolff@users.noreply.github.com>
2012-05-03 14:30:04 +0000

    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@9346 3260e6d1-4efc-4170-b0a7-36055960796d

--- OCL_core.thy
+++ OCL_core.thy
@@ -879,10 +879,24 @@ by(auto simp: not_def OclValid_def true_def invalid_def defined_def false_def
                  bot_fun_def bot_option_def null_fun_def null_option_def
         split: bool.split_asm HOL.split_if_asm option.split option.split_asm)
 
-
+nitpick_params
+ [timeout = 30, tac_timeout = 0.5, show_datatypes,assms=true]
 lemma ocl_and_defargs: 
 "\<tau> \<Turnstile> (P and Q) \<Longrightarrow> (\<tau> \<Turnstile> \<delta> P) \<and> (\<tau> \<Turnstile> \<delta> Q)"
 by(auto dest: foundation5 foundation6)
+
+(*
+nitpick_params
+nitpick_params
+ [timeout = 30, tac_timeout = 0.5, show_datatypes]
+lemma ocl_and_defargs: 
+"\<tau> \<Turnstile> ((P and Q)  \<triangleq>  (P or Q))"
+nitpick[show_all]
+
+oops
+lemma "P & Q = Q"
+nitpick
+*)
 
 text{* So far, we have only one strict Boolean predicate (-family): The strict equality. *}
 
