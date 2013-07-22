c4b04e2719bbc5a2320c676e1aff13b50e839c00
Frédéric Tuong <tuong@users.noreply.github.com>
2013-07-22 08:08:49 +0000

    prove the framing theorem
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@9800 3260e6d1-4efc-4170-b0a7-36055960796d

--- src/OCL_state.thy
+++ src/OCL_state.thy
@@ -443,23 +443,50 @@ where "X->oclIsModifiedOnly() \<equiv> (\<lambda>(\<sigma>,\<sigma>').  let  X' 
                                                then \<lfloor>\<lfloor>\<forall> x \<in> S. (heap \<sigma>) x = (heap \<sigma>') x\<rfloor>\<rfloor>
                                                else invalid (\<sigma>,\<sigma>'))"
 
-
-definition atSelf :: "('\<AA>::object,'\<alpha>::{null,object})val \<Rightarrow>
-                      ('\<AA> \<Rightarrow> '\<alpha>) \<Rightarrow>
-                      ('\<AA>::object,'\<alpha>::{null,object})val" ("(_)@pre(_)")
-where "x @pre H = (\<lambda>\<tau> . if (\<delta> x) \<tau> = true \<tau> 
+definition "atSelf x H fst_snd = (\<lambda>\<tau> . if (\<delta> x) \<tau> = true \<tau>
                         then if oid_of (x \<tau>) \<in> dom(heap(fst \<tau>)) \<and> oid_of (x \<tau>) \<in> dom(heap (snd \<tau>))
-                             then  H \<lceil>(heap(fst \<tau>))(oid_of (x \<tau>))\<rceil>
+                             then  H \<lceil>(heap(fst_snd \<tau>))(oid_of (x \<tau>))\<rceil>
                              else invalid \<tau>
                         else invalid \<tau>)"
+lemmas [simp] = atSelf_def
 
+definition atpre_Self :: "('\<AA>::object,'\<alpha>::{null,object})val \<Rightarrow>
+                      ('\<AA> \<Rightarrow> '\<alpha>) \<Rightarrow>
+                      ('\<AA>::object,'\<alpha>::{null,object})val" ("(_)@pre(_)")
+where "x @pre H = atSelf x H fst"
+
+definition atpost_Self :: "('\<AA>::object,'\<alpha>::{null,object})val \<Rightarrow>
+                      ('\<AA> \<Rightarrow> '\<alpha>) \<Rightarrow>
+                      ('\<AA>::object,'\<alpha>::{null,object})val" ("(_)@post(_)")
+where "x @post H = atSelf x H snd"
 
 theorem framing:
-      assumes modifiesclause:"\<tau> \<Turnstile> (X->excluding(x))->oclIsModifiedOnly()"
-      and    represented_x: "\<tau> \<Turnstile> \<delta>(x @pre H)"
-      and    H_is_typerepr: "inj H"
-      shows "\<tau> \<Turnstile> (x  \<triangleq>  (x @pre H))"
-sorry
+      assumes modifiesclause:"\<tau> \<Turnstile> (X->excluding(x :: ('\<AA>::object,'\<alpha>::{null,object})val))->oclIsModifiedOnly()"
+      and    represented_x: "\<tau> \<Turnstile> \<delta>(x @pre (H::('\<AA> \<Rightarrow> '\<alpha>)))"
+      and oid_is_typerepr : "inj (oid_of :: '\<alpha> \<Rightarrow> _)"
+      shows "\<tau> \<Turnstile> (x @pre H  \<triangleq>  (x @post H))"
+proof -
+ have def_x : "\<tau> \<Turnstile> \<delta> x"
+ by(insert represented_x, simp add: defined_def OclValid_def null_fun_def bot_fun_def false_def true_def atpre_Self_def invalid_def split: split_if_asm)
+ show ?thesis
+  apply(simp add:StrongEq_def OclValid_def true_def atpre_Self_def atpost_Self_def def_x[simplified OclValid_def])
+  apply(rule conjI, rule impI)
+  apply(rule_tac f = "\<lambda>x. H \<lceil>x\<rceil>" in arg_cong)
+  apply(insert modifiesclause[simplified oclismodified_def OclValid_def])
+  apply(case_tac \<tau>, rename_tac \<sigma> \<sigma>', simp split: split_if_asm)
+  apply(simp add: OclExcluding_def)
+  apply(drule foundation5[simplified OclValid_def true_def], simp)
+  apply(subst (asm) Abs_Set_0_inverse, simp)
+  apply(rule disjI2)+
+  apply (metis (hide_lams, no_types) DiffD1 OclValid_def Set_inv_lemma def_x foundation16 foundation18')
+  apply(simp)
+  apply(erule_tac x = "oid_of (x (\<sigma>, \<sigma>'))" in ballE) apply simp
+  apply(subst (asm) image_set_diff, simp add: oid_is_typerepr)
+  apply(simp)
+  apply(simp add: invalid_def bot_option_def)
 
+  apply(blast)
+ done
+qed
 
 end
