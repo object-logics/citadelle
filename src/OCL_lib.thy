52efa06b92dfeae3fd37f2e68fc9411f61f4315c
Frédéric Tuong <tuong@users.noreply.github.com>
2013-07-31 14:02:00 +0000

    support the UnlimitedNatural basic type
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@9826 3260e6d1-4efc-4170-b0a7-36055960796d

--- src/OCL_lib.thy
+++ src/OCL_lib.thy
@@ -47,7 +47,7 @@ theory OCL_lib
 imports OCL_core
 begin
 
-section{* Basic Types: Void and Integer *}
+section{* Basic Types: Void, Integer, UnlimitedNatural *}
 subsection{* The construction of the Void Type *}
 (* subsection{* Basic Constants *} *)
 type_synonym ('\<AA>)Void = "('\<AA>,unit option) val"
@@ -155,6 +155,152 @@ value "  \<tau>\<^isub>0 \<Turnstile> ( \<nine> \<le>\<^isub>o\<^isub>c\<^isub>l
 value "  \<tau>\<^isub>0 \<Turnstile> (( \<four> +\<^isub>o\<^isub>c\<^isub>l \<four> ) \<le>\<^isub>o\<^isub>c\<^isub>l \<one>\<zero> )"
 value "\<not>(\<tau>\<^isub>0 \<Turnstile> (( \<four> +\<^isub>o\<^isub>c\<^isub>l ( \<four> +\<^isub>o\<^isub>c\<^isub>l \<four> )) <\<^isub>o\<^isub>c\<^isub>l \<one>\<zero> ))"
 
+subsection{* The construction of the UnlimitedNatural Type *}
+
+text{* Unlike @{term "Integer"}, we should also include the \<infinity> value besides @{term "undefined"} and @{term "null"}. *}
+
+
+class      infinity = null +
+   fixes   infinity :: "'a"
+   assumes infinity_is_valid : "infinity \<noteq> bot"
+   assumes infinity_is_defined : "infinity \<noteq> null"
+
+instantiation   option  :: (null)infinity
+begin 
+   definition infinity_option_def: "(infinity::'a\<Colon>null option) \<equiv>  \<lfloor> null \<rfloor>"
+   instance proof  show "(infinity::'a\<Colon>null option) \<noteq> null"
+                   by( simp add:infinity_option_def null_is_valid null_option_def bot_option_def)
+                   show "(infinity::'a\<Colon>null option) \<noteq> bot"
+                   by( simp add:infinity_option_def null_option_def bot_option_def)
+            qed
+end
+
+instantiation "fun"  :: (type,infinity) infinity 
+begin
+ definition infinity_fun_def: "(infinity::'a \<Rightarrow> 'b::infinity) \<equiv> (\<lambda> x. infinity)"
+
+ instance proof 
+            show "(infinity::'a \<Rightarrow> 'b::infinity) \<noteq> bot"
+              apply(auto simp: infinity_fun_def bot_fun_def)
+              apply(drule_tac x=x in fun_cong)
+              apply(erule contrapos_pp, simp add: infinity_is_valid)
+            done
+            show "(infinity::'a \<Rightarrow> 'b::infinity) \<noteq> null"
+              apply(auto simp: infinity_fun_def null_fun_def)
+              apply(drule_tac x=x in fun_cong)
+              apply(erule contrapos_pp, simp add: infinity_is_defined)
+            done
+          qed
+end
+
+type_synonym ('\<AA>,'\<alpha>) val' = "'\<AA> st \<Rightarrow> '\<alpha>::infinity"
+
+definition limitedNatural :: "('\<AA>,'a::infinity)val' \<Rightarrow> ('\<AA>)Boolean" ("\<mu> _" [100]100)
+where   "\<mu> X \<equiv>  \<lambda> \<tau> . if X \<tau> = bot \<tau> \<or> X \<tau> = null \<tau> \<or> X \<tau> = infinity \<tau> then false \<tau> else true \<tau>"
+
+lemma (*valid*)[simp]: "\<upsilon> infinity = true"
+  by(rule ext, simp add: bot_fun_def infinity_fun_def infinity_is_valid valid_def)
+
+lemma (*defined*)[simp]: "\<delta> infinity = true"
+  by(rule ext, simp add: bot_fun_def defined_def infinity_fun_def infinity_is_defined infinity_is_valid null_fun_def)
+
+lemma (*limitedNatural*)[simp]: "\<mu> invalid = false"
+  by(rule ext, simp add: bot_fun_def invalid_def limitedNatural_def)
+
+lemma (*limitedNatural*)[simp]: "\<mu> null = false"
+  by(rule ext, simp add: limitedNatural_def) 
+
+lemma (*limitedNatural*)[simp]: "\<mu> infinity = false"
+  by(rule ext, simp add: limitedNatural_def) 
+
+type_synonym ('\<AA>)UnlimitedNatural = "('\<AA>, nat option option option) val'"
+
+locale OclUnlimitedNatural
+begin
+definition OclNat0 ::"('\<AA>)UnlimitedNatural" ("\<zero>")
+where      "\<zero> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>0::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat1 ::"('\<AA>)UnlimitedNatural" ("\<one>")
+where      "\<one> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>1::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat2 ::"('\<AA>)UnlimitedNatural" ("\<two>")
+where      "\<two> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>2::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat3 ::"('\<AA>)UnlimitedNatural" ("\<three>")
+where      "\<three> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>3::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat4 ::"('\<AA>)UnlimitedNatural" ("\<four>")
+where      "\<four> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>4::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat5 ::"('\<AA>)UnlimitedNatural" ("\<five>")
+where      "\<five> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>5::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat6 ::"('\<AA>)UnlimitedNatural" ("\<six>")
+where      "\<six> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>6::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat7 ::"('\<AA>)UnlimitedNatural" ("\<seven>")
+where      "\<seven> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>7::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat8 ::"('\<AA>)UnlimitedNatural" ("\<eight>")
+where      "\<eight> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>8::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat9 ::"('\<AA>)UnlimitedNatural" ("\<nine>")
+where      "\<nine> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>9::nat\<rfloor>\<rfloor>\<rfloor>)"
+
+definition OclNat10 ::"('\<AA>)UnlimitedNatural" ("\<one>\<zero>")
+where      "\<one>\<zero> = (\<lambda> _ . \<lfloor>\<lfloor>\<lfloor>10::nat\<rfloor>\<rfloor>\<rfloor>)"
+end
+
+definition OclNat_infinity :: "('\<AA>)UnlimitedNatural" ("\<infinity>")
+where "\<infinity> = (\<lambda>_. \<lfloor>\<lfloor>None\<rfloor>\<rfloor>)"
+
+subsection{* Validity and Definedness Properties *}
+
+lemma  "\<delta>(null::('\<AA>)UnlimitedNatural) = false" by simp
+lemma  "\<upsilon>(null::('\<AA>)UnlimitedNatural) = true"  by simp
+
+lemma [simp,code_unfold]: "\<delta> (\<lambda>_. \<lfloor>\<lfloor>\<lfloor>n\<rfloor>\<rfloor>\<rfloor>) = true"
+by(simp)
+
+lemma [simp,code_unfold]: "\<upsilon> (\<lambda>_. \<lfloor>\<lfloor>\<lfloor>n\<rfloor>\<rfloor>\<rfloor>) = true"
+by(simp)
+
+lemma [simp,code_unfold]: "\<mu> (\<lambda>_. \<lfloor>\<lfloor>\<lfloor>n\<rfloor>\<rfloor>\<rfloor>) = true"
+by(simp add: limitedNatural_def true_def
+               bot_fun_def bot_option_def null_fun_def null_option_def infinity_fun_def infinity_option_def)
+
+subsection{* Arithmetical Operations on UnlimitedNatural *}
+
+subsubsection{* Definition *}
+
+definition OclAdd\<^isub>U\<^isub>n\<^isub>l\<^isub>i\<^isub>m\<^isub>i\<^isub>t\<^isub>e\<^isub>d\<^isub>N\<^isub>a\<^isub>t\<^isub>u\<^isub>r\<^isub>a\<^isub>l ::"('\<AA>)UnlimitedNatural \<Rightarrow> ('\<AA>)UnlimitedNatural \<Rightarrow> ('\<AA>)UnlimitedNatural" (infix "+\<^isub>o\<^isub>c\<^isub>l" 40)
+where "x +\<^isub>o\<^isub>c\<^isub>l y \<equiv> \<lambda> \<tau>. if (\<mu> x) \<tau> = true \<tau> \<and> (\<mu> y) \<tau> = true \<tau>
+                then \<lfloor>\<lfloor>\<lfloor>\<lceil>\<lceil>\<lceil>x \<tau>\<rceil>\<rceil>\<rceil> + \<lceil>\<lceil>\<lceil>y \<tau>\<rceil>\<rceil>\<rceil>\<rfloor>\<rfloor>\<rfloor>
+                else invalid \<tau> "
+
+definition OclLess\<^isub>U\<^isub>n\<^isub>l\<^isub>i\<^isub>m\<^isub>i\<^isub>t\<^isub>e\<^isub>d\<^isub>N\<^isub>a\<^isub>t\<^isub>u\<^isub>r\<^isub>a\<^isub>l ::"('\<AA>)UnlimitedNatural \<Rightarrow> ('\<AA>)UnlimitedNatural \<Rightarrow> ('\<AA>)Boolean" (infix "<\<^isub>o\<^isub>c\<^isub>l" 40)
+where "x <\<^isub>o\<^isub>c\<^isub>l y \<equiv> \<lambda> \<tau>. if (\<mu> x) \<tau> = true \<tau> \<and> (\<mu> y) \<tau> = true \<tau>
+                then \<lfloor>\<lfloor>\<lceil>\<lceil>\<lceil>x \<tau>\<rceil>\<rceil>\<rceil> < \<lceil>\<lceil>\<lceil>y \<tau>\<rceil>\<rceil>\<rceil>\<rfloor>\<rfloor>
+                else if (\<delta> x) \<tau> = true \<tau> \<and> (\<delta> y) \<tau> = true \<tau>
+                then (\<mu> x) \<tau>
+                else invalid \<tau>"
+
+definition OclLe\<^isub>U\<^isub>n\<^isub>l\<^isub>i\<^isub>m\<^isub>i\<^isub>t\<^isub>e\<^isub>d\<^isub>N\<^isub>a\<^isub>t\<^isub>u\<^isub>r\<^isub>a\<^isub>l ::"('\<AA>)UnlimitedNatural \<Rightarrow> ('\<AA>)UnlimitedNatural \<Rightarrow> ('\<AA>)Boolean" (infix "\<le>\<^isub>o\<^isub>c\<^isub>l" 40)
+where "x \<le>\<^isub>o\<^isub>c\<^isub>l y \<equiv> \<lambda> \<tau>. if (\<mu> x) \<tau> = true \<tau> \<and> (\<mu> y) \<tau> = true \<tau>
+                then \<lfloor>\<lfloor>\<lceil>\<lceil>\<lceil>x \<tau>\<rceil>\<rceil>\<rceil> \<le> \<lceil>\<lceil>\<lceil>y \<tau>\<rceil>\<rceil>\<rceil>\<rfloor>\<rfloor>
+                else if (\<delta> x) \<tau> = true \<tau> \<and> (\<delta> y) \<tau> = true \<tau>
+                then not (\<mu> y) \<tau>
+                else invalid \<tau>"
+
+subsubsection{* Test Statements *}
+text{* Here follows a list of code-examples, that explain the meanings
+of the above definitions by compilation to code and execution to @{term "True"}.*}
+
+value "  \<tau>\<^isub>0 \<Turnstile> not (\<upsilon> (invalid \<le>\<^isub>o\<^isub>c\<^isub>l \<infinity>)) "
+value "  \<tau>\<^isub>0 \<Turnstile> not (\<upsilon> (null \<le>\<^isub>o\<^isub>c\<^isub>l \<infinity>)) "
+value "  \<tau>\<^isub>0 \<Turnstile>      \<upsilon> \<infinity> "
+value "  \<tau>\<^isub>0 \<Turnstile>      \<delta> \<infinity> "
+value "  \<tau>\<^isub>0 \<Turnstile> not (\<mu> \<infinity>) "
 
 section{* Fundamental Predicates on Boolean and Integer: Strict Equality *}
 
