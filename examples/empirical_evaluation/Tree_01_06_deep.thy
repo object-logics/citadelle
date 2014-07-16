dfc02dfb7e977c25ed94dddcc7e06887c351a7b6
Frédéric Tuong <tuong@users.noreply.github.com>
2014-07-16 13:59:58 +0000

    evaluate empirically
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@10442 3260e6d1-4efc-4170-b0a7-36055960796d

--- examples/empirical_evaluation/Tree_01_06_deep.thy
+++ examples/empirical_evaluation/Tree_01_06_deep.thy
@@ -3,7 +3,8 @@ generation_syntax [ deep
                       (generation_semantics [ analysis (*, oid_start 10*) ])
                       skip_export
                       (THEORY Tree_01_06_generated)
-                      (IMPORTS ["../../../src/OCL_main", "../../../src/OCL_class_diagram_static"])
+                      (IMPORTS ["../../../src/OCL_main", "../../../src/OCL_class_diagram_static"]
+                               "../../../src/OCL_class_diagram_generator")
                       SECTION
                       [ in SML module_name M (no_signatures) ]
                       (output_directory "./doc") ]
