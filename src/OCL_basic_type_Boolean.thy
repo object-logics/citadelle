7652e3b661b722e7c2981affae17ed6f5e91137b
Frédéric Tuong <tuong@users.noreply.github.com>
2014-08-01 13:53:26 +0000

    split OCL_lib
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@10509 3260e6d1-4efc-4170-b0a7-36055960796d

---
+++ src/OCL_basic_type_Boolean.thy
@@ -0,0 +1,132 @@
+(*****************************************************************************
+ * Featherweight-OCL --- A Formal Semantics for UML-OCL Version OCL 2.4
+ *                       for the OMG Standard.
+ *                       http://www.brucker.ch/projects/hol-testgen/
+ *
+ * OCL_basic_type_Boolean.thy --- Library definitions.
+ * This file is part of HOL-TestGen.
+ *
+ * Copyright (c) 2012-2014 Université Paris-Sud, France
+ *               2013-2014 IRT SystemX, France
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of the copyright holders nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************)
+(* $Id:$ *)
+
+header{* ... *}
+
+theory  OCL_basic_type_Boolean
+imports OCL_lib_common
+begin
+
+section{* Fundamental Predicates on Basic Types: Strict Equality *}
+
+subsection{* Definition *}
+
+subsection{* Logic and Algebraic Layer on Basic Types *}
+
+subsubsection{* Validity and Definedness Properties (I) *}
+
+lemma StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n_defined_args_valid:
+"(\<tau> \<Turnstile> \<delta>((x::('\<AA>)Boolean) \<doteq> y)) = ((\<tau> \<Turnstile>(\<upsilon> x)) \<and> (\<tau> \<Turnstile>(\<upsilon> y)))"
+by(auto simp: StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n OclValid_def true_def valid_def false_def StrongEq_def
+              defined_def invalid_def null_fun_def bot_fun_def null_option_def bot_option_def
+        split: bool.split_asm HOL.split_if_asm option.split)
+
+subsubsection{* Validity and Definedness Properties (II) *}
+
+lemma StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n_defargs:
+"\<tau> \<Turnstile> ((x::('\<AA>)Boolean) \<doteq> y) \<Longrightarrow> (\<tau> \<Turnstile> (\<upsilon> x)) \<and> (\<tau> \<Turnstile>(\<upsilon> y))"
+by(simp add: StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n OclValid_def true_def invalid_def
+             bot_option_def
+        split: bool.split_asm HOL.split_if_asm)
+
+subsubsection{* Validity and Definedness Properties (III) Miscellaneous *}
+
+lemma StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n_strict'' : "\<delta> ((x::('\<AA>)Boolean) \<doteq> y) = (\<upsilon>(x) and \<upsilon>(y))"
+by(auto intro!: transform2_rev defined_and_I simp:foundation10 StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n_defined_args_valid)
+
+subsubsection{* Reflexivity *}
+
+lemma StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n_refl[simp,code_unfold] :
+"((x::('\<AA>)Boolean) \<doteq> x) = (if (\<upsilon> x) then true else invalid endif)"
+by(rule ext, simp add: StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n OclIf_def)
+
+subsubsection{* Execution with Invalid or Null as Argument *}
+
+lemma StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n_strict1[simp,code_unfold] : "((x::('\<AA>)Boolean) \<doteq> invalid) = invalid"
+by(rule ext, simp add: StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n true_def false_def)
+
+lemma StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n_strict2[simp,code_unfold] : "(invalid \<doteq> (x::('\<AA>)Boolean)) = invalid"
+by(rule ext, simp add: StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n true_def false_def)
+
+subsubsection{* Const *}
+
+subsubsection{* Behavior vs StrongEq *}
+
+lemma StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n_vs_StrongEq:
+"\<tau> \<Turnstile>(\<upsilon> x) \<Longrightarrow> \<tau> \<Turnstile>(\<upsilon> y) \<Longrightarrow> (\<tau> \<Turnstile> (((x::('\<AA>)Boolean) \<doteq> y) \<triangleq> (x \<triangleq> y)))"
+apply(simp add: StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n OclValid_def)
+apply(subst cp_StrongEq[of _ "(x \<triangleq> y)"])
+by simp
+
+
+subsubsection{* Context Passing *}
+
+lemma cp_StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n:
+"((X::('\<AA>)Boolean) \<doteq> Y) \<tau> = ((\<lambda> _. X \<tau>) \<doteq> (\<lambda> _. Y \<tau>)) \<tau>"
+by(auto simp: StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n StrongEq_def defined_def valid_def  cp_defined[symmetric])
+
+lemmas cp_intro'\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n[intro!,simp,code_unfold] =
+       cp_StrictRefEq\<^sub>B\<^sub>o\<^sub>o\<^sub>l\<^sub>e\<^sub>a\<^sub>n[THEN allI[THEN allI[THEN allI[THEN cpI2]], of "StrictRefEq"]]
+
+
+subsection{* Test Statements on Basic Types. *}
+text{* Here follows a list of code-examples, that explain the meanings
+of the above definitions by compilation to code and execution to @{term "True"}.*}
+
+text{* Elementary computations on Boolean *}
+Assert "\<tau> \<Turnstile> \<upsilon>(true)"
+Assert "\<tau> \<Turnstile> \<delta>(false)"
+Assert "\<not>(\<tau> \<Turnstile> \<delta>(null))"
+Assert "\<not>(\<tau> \<Turnstile> \<delta>(invalid))"
+Assert "\<tau> \<Turnstile> \<upsilon>((null::('\<AA>)Boolean))"
+Assert "\<not>(\<tau> \<Turnstile> \<upsilon>(invalid))"
+Assert "\<tau> \<Turnstile> (true and true)"
+Assert "\<tau> \<Turnstile> (true and true \<triangleq> true)"
+Assert "\<tau> \<Turnstile> ((null or null) \<triangleq> null)"
+Assert "\<tau> \<Turnstile> ((null or null) \<doteq> null)"
+Assert "\<tau> \<Turnstile> ((true \<triangleq> false) \<triangleq> false)"
+Assert "\<tau> \<Turnstile> ((invalid \<triangleq> false) \<triangleq> false)"
+Assert "\<tau> \<Turnstile> ((invalid \<doteq> false) \<triangleq> invalid)"
+
+
+end
