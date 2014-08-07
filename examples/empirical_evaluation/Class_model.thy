49938f5bc7bb4ee6c688d1a51bbe09e7b8f14018
Frédéric Tuong <tuong@users.noreply.github.com>
2014-08-07 12:25:01 +0000

    deactivate the importation of the compiler for the shallow case
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@10519 3260e6d1-4efc-4170-b0a7-36055960796d

--- examples/empirical_evaluation/Class_model.thy
+++ examples/empirical_evaluation/Class_model.thy
@@ -140,7 +140,7 @@ definition "print_abr sprintf_int write_file =
             write_file
               (flatten [filename, ''.thy''])
               (flatten
-                [ [ flatten [''theory '', filename, '' imports '', gen_import, '' '',g,''../../src/OCL_compiler_generator_dynamic'',g,'' begin'']
+                [ [ flatten [''theory '', filename, '' imports '', gen_import,'' begin'']
                   , gen_init comp comp2]
                 , body
                 , [ ''''
@@ -153,7 +153,7 @@ definition "print_abr sprintf_int write_file =
                      , (''OCaml'', ''module_name M (no_signatures)'')
                      , (''Scala'', ''module_name M'')
                      , (''SML'', ''module_name M (no_signatures)'')])
-        , ''''
+        , flatten [ g,''../../src/OCL_compiler_generator_dynamic'',g ]
         , \<lambda> comp comp2.
             flatten_n [          ''generation_syntax [ deep''
                       ,          ''                      (generation_semantics [ analysis (*, oid_start 10*) ])''
