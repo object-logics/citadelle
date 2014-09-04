9b2a2ea0ef2a19111debb3a2add025dd08a0ab07
Frédéric Tuong <tuong@users.noreply.github.com>
2014-09-04 11:02:20 +0000

    fix r10434
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@10720 3260e6d1-4efc-4170-b0a7-36055960796d

--- src/compiler/core/OCL_compiler_core_ctxt.thy
+++ src/compiler/core/OCL_compiler_core_ctxt.thy
@@ -72,16 +72,16 @@ definition "print_ctxt_const = List_map (map_pair id Thy_consts_class) o (\<lamb
 
 definition "print_ctxt_gen_syntax_header_l l = Isab_thy (Theory_thm (Thm (List_map Thm_str l)))"
 definition "print_ctxt_gen_syntax_header f_x l ocl =
- ( let (l, ocl) = f_x l ocl in
-   if D_generation_syntax_shallow ocl then
-     l
-   else
-     Isab_thy_generation_syntax (Generation_syntax_shallow (D_design_analysis ocl))
-     # Isab_thy_ml_extended (Ml_extended (Sexpr_ocl (ocl \<lparr> D_disable_thy_output := True
-                                                         , D_file_out_path_dep := None
-                                                         , D_output_position := (0, 0) \<rparr>) ))
-     # l
- , ocl \<lparr> D_generation_syntax_shallow := True \<rparr> )"
+ (let (l, ocl) = f_x l ocl in
+  ( if D_generation_syntax_shallow ocl then
+      l
+    else
+      Isab_thy_generation_syntax (Generation_syntax_shallow (D_design_analysis ocl))
+      # Isab_thy_ml_extended (Ml_extended (Sexpr_ocl (ocl \<lparr> D_disable_thy_output := True
+                                                          , D_file_out_path_dep := None
+                                                          , D_output_position := (0, 0) \<rparr>) ))
+      # l
+  , ocl \<lparr> D_generation_syntax_shallow := True \<rparr> ))"
 
 definition "print_ctxt_pre_post_name attr_n var_at_when_hol = hol_definition (print_ctxt_const_name attr_n var_at_when_hol)"
 definition "print_ctxt_pre_post = (\<lambda>ctxt. print_ctxt_gen_syntax_header
