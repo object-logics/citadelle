2b5fed807fa227ffb1229986dd3217d18ccdc2bd
Frédéric Tuong <tuong@users.noreply.github.com>
2015-09-04 12:44:49 +0000

    continue r11914
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@11927 3260e6d1-4efc-4170-b0a7-36055960796d

--- src/compiler/meta/OCL_compiler_parser_Pure.thy
+++ src/compiler/meta/OCL_compiler_parser_Pure.thy
@@ -56,26 +56,26 @@ context i_of
 begin
 
 definition "i_of_pure_indexname a b = rec_pure_indexname
-  (ap2 a (b \<open>PureIndexname\<close>) (i_of_string a b) (i_of_nat a b))"
+  (ap2 a (b \<open>Pure_Indexname\<close>) (i_of_string a b) (i_of_nat a b))"
 
 definition "i_of_pure_class a b = rec_pure_class
-  (ap1 a (b \<open>PureClass\<close>) (i_of_string a b))"
+  (ap1 a (b \<open>Pure_Class\<close>) (i_of_string a b))"
 
 definition "i_of_pure_sort a b = rec_pure_sort
-  (ap1 a (b \<open>PureSort\<close>) (i_of_list a b (i_of_pure_class a b)))"
+  (ap1 a (b \<open>Pure_Sort\<close>) (i_of_list a b (i_of_pure_class a b)))"
 
 definition "i_of_pure_typ a b = rec_pure_typ
-  (ap2 a (b \<open>PureType\<close>) (i_of_string a b) (i_of_list a b snd))
-  (ap2 a (b \<open>PureTFree\<close>) (i_of_string a b) (i_of_pure_sort a b))
-  (ap2 a (b \<open>PureTVar\<close>) (i_of_pure_indexname a b) (i_of_pure_sort a b))"
+  (ap2 a (b \<open>Pure_Type\<close>) (i_of_string a b) (i_of_list a b snd))
+  (ap2 a (b \<open>Pure_TFree\<close>) (i_of_string a b) (i_of_pure_sort a b))
+  (ap2 a (b \<open>Pure_TVar\<close>) (i_of_pure_indexname a b) (i_of_pure_sort a b))"
 
 definition "i_of_pure_term a b = (\<lambda>f0 f1 f2 f3 f4 f5. rec_pure_term f0 f1 f2 f3 (co2 K f4) (\<lambda>_ _. f5))
-  (ap2 a (b \<open>PureConst\<close>) (i_of_string a b) (i_of_pure_typ a b))
-  (ap2 a (b \<open>PureFree\<close>) (i_of_string a b) (i_of_pure_typ a b))
-  (ap2 a (b \<open>PureVar\<close>) (i_of_pure_indexname a b) (i_of_pure_typ a b))
-  (ap1 a (b \<open>PureBound\<close>) (i_of_nat a b))
-  (ar3 a (b \<open>PureAbs\<close>) (i_of_string a b) (i_of_pure_typ a b))
-  (ar2 a (b \<open>PureApp\<close>) id)"
+  (ap2 a (b \<open>Pure_Const\<close>) (i_of_string a b) (i_of_pure_typ a b))
+  (ap2 a (b \<open>Pure_Free\<close>) (i_of_string a b) (i_of_pure_typ a b))
+  (ap2 a (b \<open>Pure_Var\<close>) (i_of_pure_indexname a b) (i_of_pure_typ a b))
+  (ap1 a (b \<open>Pure_Bound\<close>) (i_of_nat a b))
+  (ar3 a (b \<open>Pure_Abs\<close>) (i_of_string a b) (i_of_pure_typ a b))
+  (ar2 a (b \<open>Pure_App\<close>) id)"
 
 end
 
