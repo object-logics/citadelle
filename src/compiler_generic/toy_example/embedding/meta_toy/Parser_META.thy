035f93879ef95f9c9c013158015ff9bbec4fd47a
Frédéric Tuong <tuong@users.noreply.github.com>
2015-09-07 10:02:03 +0000

    alpha-rename
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@11940 3260e6d1-4efc-4170-b0a7-36055960796d

--- src/compiler_generic/toy_example/embedding/meta_toy/Parser_META.thy
+++ src/compiler_generic/toy_example/embedding/meta_toy/Parser_META.thy
@@ -216,7 +216,7 @@ definition "i_of_string_gen s_flatten s_st0 s_st a b s =
 
 definition "i_of_string = i_of_string_gen \<open>Init.S.flatten\<close>
                                           (\<lambda>s. S.flatten [\<open>(Init.ST0\<close>, s, \<open>)\<close>])
-                                          (\<lambda>s. S.flatten [\<open>(Init.abr_string.SS_base (OCL_compiler_init.string\<^sub>b\<^sub>a\<^sub>s\<^sub>e.ST\<close>, s, \<open>))\<close>])"
+                                          (\<lambda>s. S.flatten [\<open>(Init.abr_string.SS_base (Init.string\<^sub>b\<^sub>a\<^sub>s\<^sub>e.ST\<close>, s, \<open>))\<close>])"
 definition "i_of_string\<^sub>b\<^sub>a\<^sub>s\<^sub>e a b s = i_of_string_gen \<open>Init.String\<^sub>b\<^sub>a\<^sub>s\<^sub>e.flatten\<close>
                                                    (\<lambda>s. S.flatten [\<open>(Init.ST0_base\<close>, s, \<open>)\<close>])
                                                    (\<lambda>s. S.flatten [\<open>(Init.string\<^sub>b\<^sub>a\<^sub>s\<^sub>e.ST\<close>, s, \<open>)\<close>])
