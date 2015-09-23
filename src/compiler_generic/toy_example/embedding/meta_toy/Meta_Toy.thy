a968066ae2322664e2c48d138de849f33a83c95b
Frédéric Tuong <tuong@users.noreply.github.com>
2015-09-23 12:58:33 +0000

    simplify
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@12098 3260e6d1-4efc-4170-b0a7-36055960796d

--- src/compiler_generic/toy_example/embedding/meta_toy/Meta_Toy.thy
+++ src/compiler_generic/toy_example/embedding/meta_toy/Meta_Toy.thy
@@ -341,9 +341,9 @@ proof -
  by (metis Un_insert_right insert_dom)
 
  show ?thesis
-  apply(relation "measure (\<lambda>(_, rbt_inv, rbt_cycle, r).
-    ?len_merge rbt_cycle rbt_inv - length (RBT.keys rbt_cycle)
-    )", simp+)
+  apply(relation "measure (\<lambda>(_, rbt_inv, rbt_cycle, _).
+                           ?len_merge rbt_cycle rbt_inv - ?len rbt_cycle)"
+       , simp+)
   unfolding RBTS.lookup_def RBTS.insert_def
   apply(subst rbt_length, simp)
   apply(rule arith_diff)
