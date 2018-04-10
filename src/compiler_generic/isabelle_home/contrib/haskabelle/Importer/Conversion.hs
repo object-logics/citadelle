a5fe82e996161d19fad232b09893194a72907c14
Frédéric Tuong <tuong@users.noreply.github.com>
2018-04-10 14:04:36 +0000

    permute some paragraphs
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@13353 3260e6d1-4efc-4170-b0a7-36055960796d

--- src/compiler_generic/isabelle_home/contrib/haskabelle/Importer/Conversion.hs
+++ src/compiler_generic/isabelle_home/contrib/haskabelle/Importer/Conversion.hs
@@ -7,12 +7,8 @@ module Importer.Conversion (importFiles, importProject) where
 
 import Importer.Library
 
-import qualified Data.Char as Char
-import qualified Data.Data as D
-import qualified Data.Generics.Aliases as G
 import qualified Data.List as List
 import Text.PrettyPrint (render)
-import qualified Text.Printf as Printf
 
 import System.FilePath
 import System.Directory
@@ -26,6 +22,7 @@ import Importer.Printer (pprint)
 import qualified Importer.Ident_Env as Ident_Env (GlobalE)
 import qualified Importer.Isa as Isa (Module (..), ThyName (..))
 import qualified Importer.Hsx as Hsx
+import qualified Importer.Conversion.SML as SML
 
 
 importProject :: Config -> FilePath -> [String] -> IO ()
@@ -58,8 +55,8 @@ convertFiles adapt hsk_cts = do
 
   liftIO $ maybe (return ()) (\outDir -> copyFile (preludeFile adapt) (combine outDir (takeFileName (preludeFile adapt)))) outDir
   sequence_ (map (writeIsaUnit adaptTable (reservedKeywords adapt)) convertedUnits)
-  liftIO $ case outDir of Nothing -> putStrLn $ gshow (List.nubBy (let f (Isa.Module t _ _ _) = t in \m1 m2 -> f m1 == f m2)
-                                                        $ concatMap (\(IsaUnit l _ _) -> l) convertedUnits)
+  liftIO $ case outDir of Nothing -> putStrLn $ SML.gshow (List.nubBy (let f (Isa.Module t _ _ _) = t in \m1 m2 -> f m1 == f m2)
+                                                            $ concatMap (\(IsaUnit l _ _) -> l) convertedUnits)
                           _       -> return ()
 
 
@@ -84,64 +81,3 @@ writeTheory adapt reserved env thy @ (Isa.Module (Isa.ThyName thyname) _ _ _) = 
     liftIO $ writeFile dstPath content)
 
 getOutputDirMaybe f = getOutputDir >>= maybe (return ()) f
-
---
-
-render_string_wrap = True
-
-char_escape c =    c >= '\b' && c <= '\n'
-                || c == '\r'
-                || c == '"'
-
-char c =    char_escape c
-         || c >= ' ' && c <= '~' && (c /= '\\' && c /= '<' {- the escaping of string comments for future ML parsing to succeed -})
-
-type SS' = String
-type SS = SS' -> SS'
-
-monad_String = showString
-monad_Char = showChar
-monad_Drop = drop
-
-gshows :: D.Data a => a -> SS
-gshows = render `G.extQ` (render_bool :: Bool -> SS)
-                `G.extQ` (render_string :: String -> SS)
-                `G.extQ` (render_char :: Char -> SS)
-                `G.extQ` (render_int :: Int -> SS)
-                `G.extQ` (render_integer :: Integer -> SS) where
-  render_bool b = monad_String $ if b then "true" else "false"
-  render_string =
-    let (monad_String_show, monad_String') = if render_string_wrap then (monad_String' . show, \s -> monad_String ("From.string " ++ s)) else (shows, monad_String) in
-    \s -> if all char s then monad_String_show s
-          else monad_String' $ "\"" ++ concatMap (\c -> if char c && not (char_escape c) then [c]
-                                                        else Printf.printf "\\%03d" (Char.ord c)) s ++ "\""
-  render_char c = monad_Char '#' . render_string [c]
-  render_int i = if i < 0 then monad_Char '~' . shows (-i) else shows i
-  render_integer = render_int
-  render t
-    | isTuple = commaSlots' '(' "," ')'
-    | isListEmpty = monad_String "[]"
-    | isList = commaSlots '(' "::" ')'
-    | isOption = case D.showConstr . D.toConstr $ t of
-                   "Just" -> monad_String "SOME" . commaSlots '(' "," ')'
-                   "Nothing" -> monad_String "NONE"
-    | isSingleConst = constructor
-
-    | otherwise = constructor
-                . commaSlots '(' "," ')'
-
-    where constructor = monad_String . D.showConstr . D.toConstr $ t
-
-          build s = foldr (.) id . D.gmapQ ((monad_String s .) . gshows) $ t
-          commaSlots c1 s c2 = monad_Char c1 . monad_Drop (length s) . build s . monad_Char c2
-          commaSlots' c1 s c2 = -- this particular arrangement of tuples is following the ordering chosen in Haskabelle
-            case D.gmapQ gshows t of x : xs -> foldl (\x1 x2 -> monad_Char c1 . x1 . monad_String s . x2 . monad_Char c2) x xs
-          filt s = filter (not . flip elem s) (constructor "")
-
-          isTuple = all (==',') (filt "()")
-          isListEmpty = null (filt "[]")
-          isList = constructor "" == "(:)"
-          isOption = let c = constructor "" in c == "Just" || c == "Nothing"
-          isSingleConst = null (D.gmapQ (\_ -> ()) t)
-
-gshow f = gshows f ""
