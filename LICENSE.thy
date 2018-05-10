d82e221a21c3e0521d6cdaa2b43a0d6827870fb0
Frédéric Tuong <tuong@users.noreply.github.com>
2018-05-10 12:15:00 +0000

    simplify
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@13389 3260e6d1-4efc-4170-b0a7-36055960796d

--- LICENSE.thy
+++ LICENSE.thy
@@ -1,35 +1,35 @@
 theory LICENSE imports LICENSE0 begin
 
 license "3-Clause BSD" where \<open>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following
- *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
- *
- *     * Neither the name of the copyright holders nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of the copyright holders nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 \<close>
 
 country ch where \<open>Switzerland\<close>
@@ -40,99 +40,99 @@ country uk where \<open>UK\<close>
 country us where \<open>USA\<close>
 
 copyright default where \<open>
- * Copyright (c) 2011-2018 Université Paris-Saclay, Univ. Paris-Sud, France
- *               2013-2017 IRT SystemX, France
- *               2011-2015 Achim D. Brucker, Germany
- *               2016-2018 The University of Sheffield, UK
- *               2016-2017 Nanyang Technological University, Singapore
- *               2017-2018 Virginia Tech, USA
+Copyright (c) 2011-2018 Université Paris-Saclay, Univ. Paris-Sud, France
+              2013-2017 IRT SystemX, France
+              2011-2015 Achim D. Brucker, Germany
+              2016-2018 The University of Sheffield, UK
+              2016-2017 Nanyang Technological University, Singapore
+              2017-2018 Virginia Tech, USA
 \<close>
 
 project ROOT where \<open>
- * http://www.brucker.ch/projects/hol-testgen/
- * This file is part of HOL-TestGen.
+http://www.brucker.ch/projects/hol-testgen/
+This file is part of HOL-TestGen.
 \<close> default
 
 project LICENSE where \<open>
- * LICENSE
+LICENSE
 \<close> \<open>
- * Copyright (c) 2017-2018 Virginia Tech, USA
+Copyright (c) 2017-2018 Virginia Tech, USA
 \<close>
 
 project "Featherweight OCL" where \<open>
- * Featherweight-OCL --- A Formal Semantics for UML-OCL Version OCL 2.5
- *                       for the OMG Standard.
- *                       http://www.brucker.ch/projects/hol-testgen/
- *
- * This file is part of HOL-TestGen.
+Featherweight-OCL --- A Formal Semantics for UML-OCL Version OCL 2.5
+                      for the OMG Standard.
+                      http://www.brucker.ch/projects/hol-testgen/
+
+This file is part of HOL-TestGen.
 \<close> default
 
 project Citadelle where \<open>
- * Citadelle
+Citadelle
 \<close> default
 
 project Isabelle_Meta_Model where \<open>
- * A Meta-Model for the Isabelle API
+A Meta-Model for the Isabelle API
 \<close> default
 
 project Isabelle where \<open>
- * ISABELLE COPYRIGHT NOTICE, LICENCE AND DISCLAIMER.
+ISABELLE COPYRIGHT NOTICE, LICENCE AND DISCLAIMER.
 \<close> \<open>
- * Copyright (c) 1986-2016 University of Cambridge,
- *                         Technische Universitaet Muenchen,
- *                         and contributors.
- *               2013-2016 Université Paris-Saclay, Univ. Paris-Sud, France
- *               2013-2016 IRT SystemX, France
+Copyright (c) 1986-2016 University of Cambridge,
+                        Technische Universitaet Muenchen,
+                        and contributors.
+              2013-2016 Université Paris-Saclay, Univ. Paris-Sud, France
+              2013-2016 IRT SystemX, France
 \<close>
 
 project Haskabelle where \<open>
- * Haskabelle --- Converting Haskell Source Files to Isabelle/HOL Theories.
- *                http://isabelle.in.tum.de/repos/haskabelle
+Haskabelle --- Converting Haskell Source Files to Isabelle/HOL Theories.
+               http://isabelle.in.tum.de/repos/haskabelle
 \<close> \<open>
- * Copyright (c) 2007-2015 Technische Universität München, Germany
- *               2017-2018 Virginia Tech, USA
+Copyright (c) 2007-2015 Technische Universität München, Germany
+              2017-2018 Virginia Tech, USA
 \<close>
 
 project "HOL-OCL" where \<open>
- * HOL-OCL
+HOL-OCL
 \<close> default
 
 project "HOL-TOY" where \<open>
- * HOL-TOY
+HOL-TOY
 \<close> default
 
 project C11 where \<open>
- * Language.C
- * https://hackage.haskell.org/package/language-c
+Language.C
+https://hackage.haskell.org/package/language-c
 \<close> \<open>
- * Copyright (c) 1999-2017 Manuel M T Chakravarty
- *                         Duncan Coutts
- *                         Benedikt Huber
- * Portions Copyright (c) 1989,1990  James A. Roskind
+Copyright (c) 1999-2017 Manuel M T Chakravarty
+                        Duncan Coutts
+                        Benedikt Huber
+Portions Copyright (c) 1989,1990  James A. Roskind
 \<close>,\<open>
- * Language.C.Comments
- * https://hackage.haskell.org/package/language-c-comments
+Language.C.Comments
+https://hackage.haskell.org/package/language-c-comments
 \<close> \<open>
- * Copyright (c) 2010-2014 Geoff Hulette
+Copyright (c) 2010-2014 Geoff Hulette
 \<close>,\<open>
- * Securify & Orca
+Securify & Orca
 \<close> \<open>
- * Copyright (c) 2016-2017 Nanyang Technological University, Singapore
- *               2017-2018 Virginia Tech, USA
+Copyright (c) 2016-2017 Nanyang Technological University, Singapore
+              2017-2018 Virginia Tech, USA
 \<close>
 
 project Miscellaneous_Monads where \<open>
- * HOL-TestGen --- theorem-prover based test case generation
- *                 http://www.brucker.ch/projects/hol-testgen/
- *
- * Monads.thy --- a base testing theory for sequential computations.
- * This file is part of HOL-TestGen.
+HOL-TestGen --- theorem-prover based test case generation
+                http://www.brucker.ch/projects/hol-testgen/
+
+Monads.thy --- a base testing theory for sequential computations.
+This file is part of HOL-TestGen.
 \<close> \<open>
- * Copyright (c) 2005-2007, ETH Zurich, Switzerland
- *               2009 B. Wolff, Univ. Paris-Saclay, Univ. Paris-Sud, France
- *               2009, 2012 Achim D. Brucker, Germany
- *               2013-2016 Université Paris-Saclay, Univ. Paris-Sud, France
- *               2013-2016 IRT SystemX, France
+Copyright (c) 2005-2007, ETH Zurich, Switzerland
+              2009 B. Wolff, Univ. Paris-Saclay, Univ. Paris-Sud, France
+              2009, 2012 Achim D. Brucker, Germany
+              2013-2016 Université Paris-Saclay, Univ. Paris-Sud, France
+              2013-2016 IRT SystemX, France
 \<close>
 
 check_license
