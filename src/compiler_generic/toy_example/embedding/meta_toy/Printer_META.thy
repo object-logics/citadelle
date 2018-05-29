5354f411ab18148e7873a77faefa7f6f9fc51c50
Frédéric Tuong <tuong@users.noreply.github.com>
2018-05-29 20:20:14 +0000

    continue r13398
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@13402 3260e6d1-4efc-4170-b0a7-36055960796d

--- src/compiler_generic/toy_example/embedding/meta_toy/Printer_META.thy
+++ src/compiler_generic/toy_example/embedding/meta_toy/Printer_META.thy
@@ -107,11 +107,11 @@ definition "of_boot_setup_env env = (\<lambda> Boot_setup_env e \<Rightarrow>
   of_setup
     env
     (Setup
-      (SML.app
+      (SML.app0
         \<open>Generation_mode.update_compiler_config\<close>
         [ SML.app
             \<open>K\<close>
-            [ SML_let_open
+            [ SML.let_open
                 \<open>META\<close>
                 ((* Instead of using
                     (*sml_of_compiler_env_config SML_apply (\<lambda>x. SML_basic [x]) e*)
