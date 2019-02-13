450a7583bb85bf675ee7beb333979ff888447a3d
Burkhart Wolff <Burkharrt-Wolff@users.noreply.github.com>
2019-02-13 14:08:10 +0000

    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@13587 3260e6d1-4efc-4170-b0a7-36055960796d

--- C11-FrontEnd/examples/empirical_evaluation/Tree_01_00_shallow.thy
+++
@@ -1,9 +0,0 @@
-theory Tree_01_00_shallow imports "../../src/UML_Main" "../../src/compiler/Static" "../../src/compiler/Generator_dynamic_sequential" begin
-generation_syntax [ shallow (generation_semantics [ analysis ]) ]
-
-
-(* 0 *)
-
-End!
-
-end
