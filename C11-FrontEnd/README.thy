73b3dcf9aa322f4e8abf062a14369296b53d8682
Frédéric Tuong <tuong@users.noreply.github.com>
2019-03-11 15:35:26 +0000

    continue
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@13668 3260e6d1-4efc-4170-b0a7-36055960796d

--- C11-FrontEnd/README.thy
+++ C11-FrontEnd/README.thy
@@ -1,12 +1,52 @@
-This folder contains only generated files from FronEnd-Generators.
+(******************************************************************************
+ * Generation of Language.C Grammar with ML Interface Binding
+ *
+ * Copyright (c) 2018-2019 Université Paris-Saclay, Univ. Paris-Sud, France
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of the copyright holders nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************)
+
+theory README imports Main begin text \<open>
+
+\<^dir>\<open>generated\<close> contains only generated files from FronEnd-Generators.
 
 In particular, these are:
 
--- language_c.grm, SML grm file generated from Haskell grm file.
-                   To be compiled with (modified) mlyacc. 
-                   iCF https://gitlri.lri.fr/ftuong/mlton,
-                   which includes and buils mlyacc.)
+  \<^item> \<^file>\<open>generated/language_c.grm\<close> SML grm file generated from Haskell grm file.
+                     To be compiled with (modified) mlyacc. 
+                     iCF \<^url>\<open>https://gitlri.lri.fr/ftuong/mlton\<close>,
+                     which includes and buils mlyacc.)
+  
+  \<^item> \<^file>\<open>generated/language_c.grm.sig\<close> 
+     and
+     \<^file>\<open>generated/language_c.grm.sml\<close>, generated sml files from mlyacc above.
 
--- language_c.grm.sig 
-   and
-   language_c.grm.sml, generated sml files from mlyacc above.
+\<close> end
