47538a12d8472ab735f7acc65dabe7cdb222e553
Frédéric Tuong <tuong@users.noreply.github.com>
2019-09-18 19:07:14 +0000

    synchronize with gitlab.lri.fr:ftuong/isabelle_c a86fc0f02a13307a7ba6cc97d4e91568cb168966
    git-svn-id: https://projects.brucker.ch/hol-testgen/svn/HOL-TestGen/trunk/hol-testgen@14080 3260e6d1-4efc-4170-b0a7-36055960796d

--- C11-FrontEnd/examples/C_paper.thy
+++ C11-FrontEnd/examples/C_paper.thy
@@ -46,13 +46,13 @@ ML\<open>
 local
 val command = C_Inner_Syntax.command C_Inner_Isar_Cmd.setup' C_Parse.ML_source
 in
-val _ = Theory.setup (   command C_Transition.Bottom_up ("ML_setup", \<^here>)
-                      #> command C_Transition.Top_down ("ML_setup\<Down>", \<^here>))
+val _ = Theory.setup (   command C_Transition.Bottom_up ("\<simeq>setup", \<^here>)
+                      #> command C_Transition.Top_down ("\<simeq>setup\<Down>", \<^here>))
 end
 
 val C' = C_Module.C'
 
-fun C_define dir name _ _ =
+fun C_def dir name _ _ =
   Context.map_theory 
     (C_Inner_Syntax.command0
       (fn src => fn context => C' (C_Stack.Data_Lang.get' context |> #2) src context)
@@ -64,12 +64,12 @@ local
 in
 val _ = Theory.setup
   (ML_Antiquotation.declaration
-    @{binding "C_define"}
+    @{binding "C_def"}
     (Scan.lift (Parse.sym_ident -- Parse.position Parse.name))
     (fn _ => fn (top_down, (name, pos)) =>
       tap (fn ctxt => Context_Position.reports ctxt [(pos, Markup.keyword1)]) #>
       C_Context.fun_decl
-               "cmd" "x" ( "C_define "
+               "cmd" "x" ( "C_def "
                          ^ (case top_down of "\<Up>" => "C_Transition.Bottom_up"
                                            | "\<Down>" => "C_Transition.Top_down"
                                            | _ => error "Illegal symbol")
@@ -88,17 +88,17 @@ section \<open>\<close>
 
 C (*NONE*) \<comment> \<open> the command starts with a default empty environment \<close>
 \<open>int f (int a)
-  //@ ++& ML_setup \<open>fn stack_top => fn env => highlight stack_top\<close>
-  { /*@ @ ML_setup \<open>fn stack_top => fn env =>
+  //@ ++& \<simeq>setup \<open>fn stack_top => fn env => highlight stack_top\<close>
+  { /*@ @ \<simeq>setup \<open>fn stack_top => fn env =>
                     C (SOME env) (* the command starts with some provided environment *)
-                     \<open>int b = a + b; //@ C1 \<open>int c; //@ @ ML_setup\<Down> \<open>@{C_define \<Up> C2}\<close> \
+                     \<open>int b = a + b; //@ C1 \<open>int c; //@ @ \<simeq>setup\<Down> \<open>@{C_def \<Up> C2}\<close> \
                                                         @ C1  \<open>//* C2 \<open>int d;\<close>\<close>        \
                                                         @ C1\<Down> \<open>//* C2 \<open>int d;\<close>\<close>        \<close>
                       int b = a + b + c + d;\<close>\<close>
-        @ ML_setup \<open>fn stack_top => fn env => C NONE \<open>#define int int
+        @ \<simeq>setup \<open>fn stack_top => fn env => C NONE \<open>#define int int
                                                       int b = a + b; //* C2 \<open>int c = b;\<close>\<close>\<close>
-          ML_setup \<open>@{C_define \<Up> (* bottom-up *)  C1  }\<close>
-          ML_setup \<open>@{C_define \<Down> (* top-down  *) "C1\<Down>"}\<close>
+          \<simeq>setup \<open>@{C_def \<Up> (* bottom-up *)  C1  }\<close>
+          \<simeq>setup \<open>@{C_def \<Down> (* top-down  *) "C1\<Down>"}\<close>
      */
     return a + b + c + d; /* explicit highlighting */ }\<close>
 
